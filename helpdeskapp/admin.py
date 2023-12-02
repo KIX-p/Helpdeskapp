c67326f9e118ecbee360b7db7ab81013c6dd41c6
Pawel Kikh <pavlokiht2005@gmail.com>
2023-12-02 10:06:21 +0100

    Add ITuser and Ticket models to admin.py

--- helpdeskapp/admin.py
+++ helpdeskapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ITuser, Ticket
+
+@admin.register(ITuser)
+class ITuserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'id']
+    list_filter = ['user']
+    search_fields = ['user__username']
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'priority', 'request_type', 'created_at', 'assigned_to', 'updated_at', 'created_by']
+    list_filter = ['status', 'priority', 'request_type', 'created_at', 'assigned_to', 'updated_at', 'created_by']
+    search_fields = ['title', 'description']
+

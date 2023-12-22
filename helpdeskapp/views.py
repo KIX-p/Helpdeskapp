1d5a6ef5b889d9ff7e104db4a55a6a207bf2cfd8
Pawel Kikh <pavlokiht2005@gmail.com>
2023-12-22 15:40:06 +0100

    Update form submit button label

--- helpdeskapp/views.py
+++ helpdeskapp/views.py
@@ -23,7 +23,6 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
-
             if user is not None:
                 if user.is_active:
                     login(request, user)
